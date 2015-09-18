462171723fde0cca406fc4ad38da18fd248d0cc9
JC Nesci <jc.nesci@gmail.com>
2015-09-18 15:49:09 -0700

    Added makeBar() func to Histogram class.

--- src/drawers.jsx
+++ src/drawers.jsx
@@ -12,12 +12,45 @@ var Histogram = React.createClass({
 	  this.update_d3(nextProps);
 	},
 	update_d3: function (props) {
+		this.histogram
+			.bins(props.bins)
+			.value(this.props.value);
 
+		var bars = this.histogram(props.data),
+			counts = bars.map(function (d) { return d.y; });
+
+		this.setState({bars: bars});
+
+		this.widthScale
+			.domain([d3.min(counts), d3.max(counts)])
+			.range([9, props.width-props.axisMargin]);
+
+		this.yScale
+			.domain([0, d3.max( bars.map(function (d) { return d.x+d.dx; }) )])
+			.range([0, props.height-props.topMargin-props.bottomMargin]);
+	},
+	makeBar: function (bar) {
+		var percent = bar.y/this.props.data.length*100;
+		var props = {
+			percent: percent,
+			x: this.props.axisMargin,
+			y: this.yScale(bar.x),
+			width: this.widthScale(bar.y),
+			height: this.yScale(bar.dx),
+			key: 'histogram-bar-'+ bar.x +'-'+ bar.y
+		};
+		return (
+			<HistogramBar {...props} />
+		);
 	},
 	render: function () {
 		var translate = 'translate(0, '+ this.props.topMargin +')';
 		return (
-			<g className='histogram' transform={translate}></g>
+			<g className='histogram' transform={translate}>
+				<g className='bars'>
+					{this.state.bars.map(this.makeBar)}
+				</g>
+			</g>
 		);
 	}
 });
